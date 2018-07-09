dfafcec2190f5c9af1ace93770fc9270d59a3162
ftcawb06 <815457143e@gmail.com>
2018-07-09 15:36:17 -0400

    Merge branch 'master' of https://github.com/TrilliumLearning/USGS_MapService into Huaqi
    # Conflicts:
    #	routes/routes.js
    #	views/userProfile.ejs

--- routes/routes.js
+++ routes/routes.js
@@ -46,17 +46,24 @@ module.exports = function (app, passport) {
     // CS APP Home Section =================
     // =====================================
     app.get('/app', function (req, res) {
-        res.render('CitySmartV2.ejs');
+        res.render('USGS.ejs');
     });
 
     // =====================================
     // LOGIN Section =======================
     // =====================================
 
+    app.get('/USGS',function (req,res) {
+        res.render('USGS.ejs');
+    });
+
+    app.get('/page?',function (req,res) {
+        res.render('Page??.ejs');
+    });
+
     app.get('/request', function (req, res) {
         res.redirect('/login');
     });
-
 
     // show the login form
     app.get('/login', function (req, res) {
@@ -227,7 +234,7 @@ module.exports = function (app, passport) {
         let pictureStr = req.query.pictureStr.split(',');
         // mover folder
         for(let i = 0; i < pictureStr.length; i++) {
-            fs.rename('./'+ upload_Dir + pictureStr[i] + '' , './' + geoData_Dir + pictureStr[i] + '', function (err) {
+            fs.rename(''+ upload_Dir + '/' + pictureStr[i] + '' , '' + geoData_Dir + '/' + pictureStr[i] + '', function (err) {
                 if (err) {
                     console.log(err);
                 } else {
@@ -485,8 +492,8 @@ module.exports = function (app, passport) {
             status: req.body.status
         };
         // console.log(newUser);
-        myStat = "INSERT INTO CitySmart.UserLogin ( username, password, userrole, dateCreated, dateModified, createdUser, status) VALUES ( '" + newUser.username + "','" + newUser.password+ "','" + newUser.userrole+ "','" + newUser.dateCreated+ "','" + newUser.dateModified+ "','" + newUser.createdUser + "','" + newUser.status + "');";
-        mylogin = "INSERT INTO CitySmart.UserProfile ( username, firstName, lastName) VALUES ('"+ newUser.username + "','" + newUser.firstName+ "','" + newUser.lastName + "');";
+        myStat = "INSERT INTO USGS.UserLogin ( username, password, userrole, dateCreated, dateModified, createdUser, status) VALUES ( '" + newUser.username + "','" + newUser.password+ "','" + newUser.userrole+ "','" + newUser.dateCreated+ "','" + newUser.dateModified+ "','" + newUser.createdUser + "','" + newUser.status + "');";
+        mylogin = "INSERT INTO USGS.UserProfile ( username, firstName, lastName) VALUES ('"+ newUser.username + "','" + newUser.firstName+ "','" + newUser.lastName + "');";
         con_CS.query(myStat + mylogin, function (err, rows) {
             //newUser.id = rows.insertId;
             if (err) {
@@ -529,8 +536,8 @@ module.exports = function (app, passport) {
             status: req.body.status
         };
 
-        myStat = "INSERT INTO CitySmart.UserLogin ( username, password, userrole, dateCreated, dateModified, createdUser, status) VALUES ( '" + newUser.username + "','" + newUser.password+ "','" + newUser.userrole+ "','" + newUser.dateCreated+ "','" + newUser.dateModified+ "','" + newUser.createdUser + "','" + newUser.status + "');";
-        mylogin = "INSERT INTO CitySmart.UserProfile ( username, firstName, lastName) VALUES ('"+ newUser.username + "','" + newUser.firstName+ "','" + newUser.lastName + "');";
+        myStat = "INSERT INTO USGS.UserLogin ( username, password, userrole, dateCreated, dateModified, createdUser, status) VALUES ( '" + newUser.username + "','" + newUser.password+ "','" + newUser.userrole+ "','" + newUser.dateCreated+ "','" + newUser.dateModified+ "','" + newUser.createdUser + "','" + newUser.status + "');";
+        mylogin = "INSERT INTO USGS.UserProfile ( username, firstName, lastName) VALUES ('"+ newUser.username + "','" + newUser.firstName+ "','" + newUser.lastName + "');";
         con_CS.query(myStat + mylogin, function (err, rows) {
             //newUser.id = rows.insertId;
             if (err) {
@@ -817,7 +824,7 @@ module.exports = function (app, passport) {
         // console.log (result);
         res.setHeader("Access-Control-Allow-Origin", "*");
 
-        var update1 = "UPDATE CitySmart.UserProfile SET ";
+        var update1 = "UPDATE USGS.UserProfile SET ";
         var update3 = " WHERE username = '" + req.user.username + "'";
         let update2 = "";
         for (let i = 0; i < result.length - 3; i++) {
@@ -867,7 +874,7 @@ module.exports = function (app, passport) {
         let filepathname = "http://localhost:63342/NASACitySmart-V2/a/" + responseDataUuid;
 
 
-        let statement2 = "INSERT INTO CitySmart.Request_Form (" + name + ") VALUES (" + valueSubmit + ");";
+        let statement2 = "INSERT INTO USGS.Request_Form (" + name + ") VALUES (" + valueSubmit + ");";
         // console.log(statement2);
 
         con_CS.query(statement2, function (err, result) {
@@ -964,11 +971,11 @@ module.exports = function (app, passport) {
         let approveIDStr = req.query.tID;
         let approvepictureStr = req.query.LUN.split(',');
 
-        let statement = "UPDATE CitySmart.Request_Form SET Status = 'Active' WHERE RID = '" + approveIDStr + "'";
+        let statement = "UPDATE USGS.Request_Form SET Status = 'Active' WHERE RID = '" + approveIDStr + "'";
 
         // mover folder
         for(let i = 0; i < approvepictureStr.length; i++) {
-            fs.rename('./'+ upload_Dir + pictureStr[i] + '' , './' + geoData_Dir + pictureStr[i] + '',  function (err) {
+            fs.rename(''+ upload_Dir +'/' + approvepictureStr[i] + '' , '' + geoData_Dir + '/' + approvepictureStr[i] + '',  function (err) {
                 if (err) {
                     console.log(err);
                 } else {
@@ -990,7 +997,7 @@ module.exports = function (app, passport) {
         // console.log(result);
         res.setHeader("Access-Control-Allow-Origin", "*");
 
-        var update1 = "UPDATE CitySmart.Request_Form SET " ;
+        var update1 = "UPDATE USGS.Request_Form SET " ;
         var update3 = " WHERE RID = '" + result[1][1] + "';";
         let update2 = "";
 
@@ -1022,9 +1029,9 @@ module.exports = function (app, passport) {
         let filepathname = "http://localhost:63342/USGS_MapService/a/" + responseDataUuid;
         console.log(valueSubmit);
         let statement1 = update1+update2+update3;
-        let statement2 = "UPDATE CitySmart.Request_Form SET Layer_Uploader = '" + valueSubmit[13] + "', 'Layer_Uploader_name = '" + valueSubmit[14] + "';";
+        let statement2 = "UPDATE USGS.Request_Form SET Layer_Uploader = '" + valueSubmit[13] + "', 'Layer_Uploader_name = '" + valueSubmit[14] + "';";
         if(result[4][2] === "other"){
-            let statement = "INSERT INTO CitySmart.LayerMenu VALUES (" + result[7][1] + "," + result[0][0] + "," + result[4][1] + "," + result[6][1] + "," + result[7][1] + "," + result[10][1] + "," + result[8][1] + "," + result[9][1] + ", 'Active');";
+            let statement = "INSERT INTO USGS.LayerMenu VALUES (" + result[7][1] + "," + result[0][0] + "," + result[4][1] + "," + result[6][1] + "," + result[7][1] + "," + result[10][1] + "," + result[8][1] + "," + result[9][1] + ", 'Active');";
             con_CS.query(statement1 + statement + statement2, function (err, result) {
                 if (err) {
                     throw err;
@@ -1033,7 +1040,7 @@ module.exports = function (app, passport) {
                 }
             });
         }else{
-            let statement = "INSERT INTO CitySmart.LayerMenu VALUES ('" + result[7][1] + "','" + result[0][1] + "','" + result[3][1] + "','" + result[5][1] + "','" + result[7][1] + "','" + result[10][1] + "','" + result[8][1] + "','" + result[9][1] + "', 'Active');";
+            let statement = "INSERT INTO USGS.LayerMenu VALUES ('" + result[7][1] + "','" + result[0][1] + "','" + result[3][1] + "','" + result[5][1] + "','" + result[7][1] + "','" + result[10][1] + "','" + result[8][1] + "','" + result[9][1] + "', 'Active');";
            con_CS.query(statement1 + statement + statement2, function (err, result) {
                 if (err) {
                     throw err;
@@ -1061,6 +1068,7 @@ module.exports = function (app, passport) {
             } else {
                 filePath0 = results[0];
                 let JSONresult = JSON.stringify(results, null, "\t");
+                console.log(JSONresult);
                 res.send(JSONresult);
                 res.end()
             }
@@ -1073,8 +1081,8 @@ module.exports = function (app, passport) {
         let transactionID = req.query.transactionIDStr.split(',');
         let pictureStr = req.query.pictureStr.split(',');
         for (let i = 0; i < transactionID.length; i++) {
-            let statement = "UPDATE CitySmart.Request_Form SET Status = 'Delete' WHERE RID = '" + transactionID[i] + "'";
-            fs.rename('./'+ geoData_Dir + pictureStr[i] + '' , './' + upload_Dir + pictureStr[i] + '',  function (err) {
+            let statement = "UPDATE USGS.Request_Form SET Status = 'Delete' WHERE RID = '" + transactionID[i] + "'";
+            fs.rename(''+ geoData_Dir + '/' + pictureStr[i] + '' , '' + upload_Dir + '/' + pictureStr[i] + '',  function (err) {
                 if (err) {
                     console.log(err);
                 } else {
@@ -1146,22 +1154,6 @@ module.exports = function (app, passport) {
             res.json(results);
             console.log(results);
         });
-    });
-
-//Delete button
-    app.get('/deleteData', function (req, res) {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        let transactionID = req.query.transactionIDStr.split(',');
-        console.log(transactionID);
-        for (let i = 0; i < transactionID.length; i++) {
-            let statement = "UPDATE CitySmart.GeneralFormDatatable SET Status = 'Delete' WHERE ID = '" + transactionID[i] + "'";
-            // console.log(statement);
-            con_CS.query(statement, function (err, results) {
-                if (err) throw err;
-                res.json(results[i]);
-            });
-        }
-
     });
 
 //AddData in table
@@ -1274,7 +1266,7 @@ module.exports = function (app, passport) {
     app.get('/createlayer', function (req, res) {
         res.setHeader("Access-Control-Allow-Origin", "*");
 
-        con_CS.query("SELECT * From CitySmart.LayerMenu", function (err, result) {
+        con_CS.query("SELECT * From USGS.LayerMenu", function (err, result) {
             console.log("recive and processing");
 
             let JSONresult = JSON.stringify(result, null, "\t");
