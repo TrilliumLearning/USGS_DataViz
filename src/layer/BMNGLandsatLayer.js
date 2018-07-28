99cbcf1d9845d9a55b4164cfe9ac7c9d87a44dff
FTCWB04 <azhao@northernacademy.org>
2018-07-28 10:25:18 -0400

    Merge branch 'Trueman' of https://github.com/TrilliumLearning/USGS_MapService
    # Conflicts:
    #	scripts/usgswt.js

--- src/layer/BMNGLandsatLayer.js
+++ src/layer/BMNGLandsatLayer.js
@@ -42,7 +42,7 @@ define([
             this.displayName = "Blue Marble & Landsat";
             this.pickEnabled = false;
 
-            this.urlBuilder = new WmsUrlBuilder("https://worldwind25.arc.nasa.gov/wms",
+            this.urlBuilder = new WmsUrlBuilder("https://cors.aworldbridgelabs.com/https://worldwind25.arc.nasa.gov/wms",
                 "BlueMarble-200405,esat", "", "1.3.0");
         };
 
