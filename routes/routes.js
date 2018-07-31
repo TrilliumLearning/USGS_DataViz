95d944824f7856ce30f6d30b4e98cb5c1ecf9e96
ftcawb06 <815457143e@gmail.com>
2018-07-31 09:10:21 -0400

    Merge branch 'master' of https://github.com/TrilliumLearning/USGS_MapService into Huaqi
    # Conflicts:
    #	scripts/OptionList.js
    #	views/userHome.ejs
    #	views/usgs_mapsvc.ejs

--- routes/routes.js
+++ routes/routes.js
@@ -949,7 +949,7 @@ module.exports = function (app, passport) {
 
         let statement2 = "INSERT INTO USGS.Request_Form (" + name + ") VALUES (" + valueSubmit + ");";
         // let statement3 = "INSERT INTO USGS.LayerUploader VALUES (" + valueSubmit[1] + " " + valueSubmit[13]
-        // console.log(statement2);
+
 
         con_CS.query(statement2, function (err, result) {
             if (err) {
